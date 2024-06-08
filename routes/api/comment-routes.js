a69788182577dd4baaf61e07f132c3dbac94ecc0
Zachary Wagner <zwag14@yahoo.com>
2024-06-07 20:29:32 -0500

    update comment controller and route and reply fuctionality with front end

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,13 +1,18 @@
 const router = require('express').Router();
 const {
   addComment,
-  removeComment
+  removeComment,
+  addReply,
+  removeReply
 } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router.route('/:pizzaId/:commentId').put(addReply).delete(removeComment);
+
+// /api/commens/<pizzaId>/<commentId>/<replyId>
+router.route('/:pizzaId/:commentId/:replyId').delete(remoreReply);
 
 module.exports = router;
